aea206afc95a4d33a03a4d75f7d07505c2e004e1
= <=>
2026-02-02 17:53:46 +0000

    recent harsh commit

--- portfolio-frontend/components/sidebar.js
+++ portfolio-frontend/components/sidebar.js
@@ -1,14 +1,5 @@
-<aside class="sidebar">
-
-    <h2>Portfolio Manager</h2>
-
-    <ul>
-        <li><a href="index.html">Dashboard</a></li>
-        <li><a href="holdings.html">Holdings</a></li>
-        <li><a href="transactions.html">Transactions</a></li>
-        <li><a href="analytics.html">Analytics</a></li>
-        <li><a href="settings.html">Settings</a></li>
-        <li><a href="subscriptions.html">Subscriptions</a></li>
-    </ul>
-
-</aside>
+fetch("components/sidebar.html")
+.then(res => res.text())
+.then(data => {
+    document.getElementById("sidebar-container").innerHTML = data;
+});
